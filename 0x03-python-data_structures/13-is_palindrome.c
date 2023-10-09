80206ef1baadeb65e9e8c12855bbd2d9a26c40f0
3become4 <al.mag31995@gmail.com>
2023-10-09 10:52:34 -0700

    i hate c

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -1,65 +1,65 @@
 #include "lists.h"
 
-listint_t *reverse_listint(listint_t **x);
-int is_palindrome(listint_t **x);
+listint_t *reverse_listint(listint_t **head);
+int is_palindrome(listint_t **head);
 
 /**
  * reverse_listint - Reverses list.
  *
- * @x: start of list
+ * @head: list start
  *
  * Return: reversed list
  */
-listint_t *reverse_listint(listint_t **x)
+listint_t *reverse_listint(listint_t **head)
 {
-	listint_t *y = *x, *next, *prev = NULL;
+	listint_t *node = *head, *next, *prev = NULL;
 
-	while (y)
+	while (node)
 	{
-		next = y->next;
-		y->next = prev;
-		prev = y;
-		y = next;
+		next = node->next;
+		node->next = prev;
+		prev = node;
+		node = next;
 	}
 
-	*x = prev;
-	return (*x);
+	*head = prev;
+	return (*head);
 }
 
 /**
- * is_palindrome - if is palindrome.
+ * is_palindrome - if palindrome.
  *
- * @x: start of list
+ * @head: list start
  *
  * Return: 0 or 1
  */
-int is_palindrome(listint_t **x)
+int is_palindrome(listint_t **head)
 {
 	listint_t *tmp, *rev, *mid;
-	size_t s = 0, i;
+	size_t size = 0, i;
 
-	if (*x == NULL || (*x)->next == NULL)
+	if (*head == NULL || (*head)->next == NULL)
 		return (1);
 
-	tmp = *x;
+	tmp = *head;
 	while (tmp)
 	{
-		s++;
+		size++;
 		tmp = tmp->next;
 	}
 
-	tmp = *x;
-	for (i = 0; i < (s / 2) - 1; i++)
+	tmp = *head;
+	for (i = 0; i < (size / 2) - 1; i++)
 		tmp = tmp->next;
 
-	if ((s % 2) == 0 && tmp->n != tmp->next->n)
+	if ((size % 2) == 0 && tmp->n != tmp->next->n)
 		return (0);
 
 	tmp = tmp->next->next;
 	rev = reverse_listint(&tmp);
 	mid = rev;
 
-	tmp = *x;
+	tmp = *head;
 	while (rev)
 	{
 		if (tmp->n != rev->n)
